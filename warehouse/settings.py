e434432a3de7193b2213ecc38f34920edd4e7063
SkylerJohnson102020 <basseuph@gmail.com>
2021-07-19 10:17:28 -0700

    gitignore created with secret key. Docker files created.

--- warehouse/settings.py
+++ warehouse/settings.py
@@ -1,3 +1,4 @@
+from secrets import *
 """
 Django settings for warehouse project.
 
@@ -15,18 +16,13 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w!6(l09z^ca_)7s#pev1ie9$)g%hg5h)ul8wbnwsw%e8x_-gsq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
